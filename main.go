ba4d590b158c77125079b3847158fee9762881b3
AloneLegion <com.berdin@gmail.com>
2020-07-03 14:33:12 +0300

    First step complete

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"github.com/alonelegion/musicstore_rest_api/controllers"
 	"github.com/alonelegion/musicstore_rest_api/models"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	route := gin.Default()
 
+	// Подключение к базе данных
 	models.ConnectDB()
 
-	route.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
-	})
+	// Маршруты
+	route.GET("/tracks", controllers.GetAllTracks)
+	route.POST("/tracks", controllers.CreateTrack)
+	route.GET("/tracks/:id", controllers.GetTrack)
+	route.PATCH("/tracks/:id", controllers.UpdateTrack)
+	route.DELETE("/tracks/:id", controllers.DeleteTrack)
 
+	// Запуск сервера
 	route.Run()
 }
--- models/database.go
+++ models/database.go
@@ -5,12 +5,14 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func ConnectDB() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=musicstore password=postgres sslmode=disable")
+var DB *gorm.DB
+
+func ConnectDB() {
+	db, err := gorm.Open("postgres", "host=database-1.c1fzlyaabojw.eu-central-1.rds.amazonaws.com port=5432 user=postgres dbname=musicstore password=zw345b7u sslmode=disable")
 	if err != nil {
 		panic("Не удалось подключиться к базе данных")
 	}
 	db.AutoMigrate(&Track{})
 
-	return db
+	DB = db
 }
